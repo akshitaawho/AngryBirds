0ec591a6d6b3d068bfe61f962f91300221d8778a
akshitaawho <163016409+akshitaawho@users.noreply.github.com>
2024-10-23 00:53:26 +0530

    Update PlayScreen.java

--- PlayScreen.java
+++ PlayScreen.java
@@ -6,18 +6,14 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class PlayScreen implements Screen {
     private Main game;
-    //Texture texture;
     private OrthographicCamera gamecam;
     private Viewport gamePort;
     private Hud hud;
@@ -28,49 +24,42 @@ public class PlayScreen implements Screen {
 
     public PlayScreen(Main game) {
         this.game = game;
-        //texture = new Texture("level1bg.jpg");
         gamecam = new OrthographicCamera();
-        gamePort = new FitViewport(Main.V_WIDTH, Main.V_HEIGHT, gamecam);
-        hud = new Hud(game.batch);
+
+        // Load the map first before accessing its properties
         maploader = new TmxMapLoader();
         map = maploader.load("level1.tmx");
-        renderer = new OrthogonalTiledMapRenderer(map);
-        gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
 
+        // Now calculate map width and height after loading the map
+        float mapw = map.getProperties().get("width", Integer.class) * 16;
+        float maph = map.getProperties().get("height", Integer.class) * 16;
+
+        // Initialize the viewport with map width and height
+        gamePort = new FitViewport(mapw, maph, gamecam);
+
+        // Center the camera on the map
+        gamecam.position.set(mapw / 2, maph / 2, 0);
+
+        hud = new Hud(game.batch);
+        renderer = new OrthogonalTiledMapRenderer(map);
     }
 
     @Override
-    public void show() {
-
-    }
-
-    public void handleInput(float dt) {
-        if (Gdx.input.isTouched()) {
-            gamecam.position.x += 100 * dt;
-        }
-    }
-
-    public void update(float dt) {
-        handleInput(dt);
-
-        gamecam.update();
-        renderer.setView(gamecam);
-    }
+    public void show() {}
 
     @Override
     public void render(float delta) {
-        update(delta);
+        gamecam.update();
+        renderer.setView(gamecam);
 
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        // Clear the screen with a background color
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        // Render the map
         renderer.render();
 
-        game.batch.setProjectionMatrix(gamecam.combined);
-        //game.batch.begin();
-        //game.batch.draw(texture, 0, 0);
-        //game.batch.end();
-
+        // Draw the HUD
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
     }
@@ -81,98 +70,18 @@ public class PlayScreen implements Screen {
     }
 
     @Override
-    public void pause() {
-
-    }
+    public void pause() {}
 
     @Override
-    public void resume() {
-
-    }
+    public void resume() {}
 
     @Override
-    public void hide() {
-
-    }
+    public void hide() {}
 
     @Override
     public void dispose() {
-
+        map.dispose();
+        renderer.dispose();
+       // hud.dispose();
     }
 }
-
-//public class PlayScreen implements Screen {
-//    private Main game;
-//    private OrthographicCamera gamecam;
-//    private Viewport gamePort;
-//    private Hud hud;
-//
-//    private TmxMapLoader maploader;
-//    private TiledMap map;
-//    private OrthogonalTiledMapRenderer renderer;
-//
-//    public PlayScreen(Main game) {
-//        this.game = game;
-//        gamecam = new OrthographicCamera();
-//        gamePort = new FitViewport(Main.V_WIDTH, Main.V_HEIGHT, gamecam);
-//        hud = new Hud(game.batch);
-//
-//        maploader = new TmxMapLoader();
-//        map = maploader.load("level1.tmx");
-//        renderer = new OrthogonalTiledMapRenderer(map);
-//
-//        gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
-//    }
-//
-//    @Override
-//    public void show() {}
-//
-//    public void handleInput(float dt) {
-//        if (Gdx.input.isTouched()) {
-//            gamecam.position.x += 100 * dt;
-//        }
-//    }
-//
-//    public void update(float dt) {
-//        handleInput(dt);
-//        gamecam.update();
-//        renderer.setView(gamecam);
-//    }
-//
-//    @Override
-//    public void render(float delta) {
-//        update(delta);
-//
-//        Gdx.gl.glClearColor(0, 0, 0, 1);
-//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-//
-//        renderer.render();
-//
-//        game.batch.setProjectionMatrix(gamecam.combined);
-//
-//        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
-//        hud.stage.draw();
-//    }
-//
-//    @Override
-//    public void resize(int width, int height) {
-//        gamePort.update(width, height);
-//        gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
-//    }
-//
-//    @Override
-//    public void pause() {}
-//
-//    @Override
-//    public void resume() {}
-//
-//    @Override
-//    public void hide() {}
-//
-//    @Override
-//    public void dispose() {
-//        map.dispose();
-//        renderer.dispose();
-//
-//    }
-//}
